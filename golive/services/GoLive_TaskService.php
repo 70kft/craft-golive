e919ec98374bf86a32b027ebea1aa9f6699ae9bf
keith <keith@70kft.com>
2015-02-19 23:27:53 -0600

    combine ssh tasks into a single function

--- golive/services/GoLive_TaskService.php
+++ golive/services/GoLive_TaskService.php
@@ -40,7 +40,7 @@ class GoLive_TaskService extends BaseApplicationComponent {
     $backup = new GoLive_DbBackup();
 
     // The settings stores excluded tables as a collection, with table as a property.
-    $excludeCollection = $this->_getSettings()['backup']['excludeTables'];
+    $excludeCollection = $this->getSettings()['backup']['excludeTables'];
 
     // Map to a simple array of table names
     $exclude = array();
@@ -69,7 +69,7 @@ class GoLive_TaskService extends BaseApplicationComponent {
    * @return bool
    */
   public function copyBackup($settings, $arg = null) {
-    $keepAfterCopy = ($this->_getSettings()['backup']['keepBackup'] === '1') ? true : false;
+    $keepAfterCopy = ($this->getSettings()['backup']['keepBackup'] === '1') ? true : false;
 
     $pathToBackup =
       craft()->path->getDbBackupPath() .
@@ -77,13 +77,13 @@ class GoLive_TaskService extends BaseApplicationComponent {
         IOHelper::cleanFilename($settings->backupFileName)
       );
 
-    $destination = $this->_getSettings()['copyBackup']['destination'];
+    $destination = $this->getSettings()['copyBackup']['destination'];
     $destination = rtrim($destination, '/') . '/';
     $destination = $destination . basename($pathToBackup);
 
-    $sshHostname = $this->_getSettings()['ssh']['remote']['hostname'];
-    $sshUsername = $this->_getSettings()['ssh']['remote']['username'];
-    $sshPassword = craft()->goLive_security->decrypt($this->_getSettings()['ssh']['remote']['password']);
+    $sshHostname = $this->getSettings()['ssh']['remote']['hostname'];
+    $sshUsername = $this->getSettings()['ssh']['remote']['username'];
+    $sshPassword = craft()->goLive_security->decrypt($this->getSettings()['ssh']['remote']['password']);
 
     $sftp = new Net_SFTP($sshHostname);
     $sftp->login($sshUsername, $sshPassword);
@@ -112,7 +112,7 @@ class GoLive_TaskService extends BaseApplicationComponent {
    * @return bool
    */
   public function importBackup($settings, $arg = null) {
-    $keepAfterCopy = ($this->_getSettings()['importBackup']['keepBackup'] === '1') ? true : false;
+    $keepAfterCopy = ($this->getSettings()['importBackup']['keepBackup'] === '1') ? true : false;
 
     $pathToBackup =
       craft()->path->getDbBackupPath() .
@@ -120,18 +120,18 @@ class GoLive_TaskService extends BaseApplicationComponent {
         IOHelper::cleanFilename($settings->backupFileName)
       );
 
-    $destination = $this->_getSettings()['copyBackup']['destination'];
+    $destination = $this->getSettings()['copyBackup']['destination'];
     $destination = rtrim($destination, '/') . '/';
     $destination = $destination . basename($pathToBackup);
 
-    $sshHostname = $this->_getSettings()['ssh']['remote']['hostname'];
-    $sshUsername = $this->_getSettings()['ssh']['remote']['username'];
-    $sshPassword = craft()->goLive_security->decrypt($this->_getSettings()['ssh']['remote']['password']);
+    $sshHostname = $this->getSettings()['ssh']['remote']['hostname'];
+    $sshUsername = $this->getSettings()['ssh']['remote']['username'];
+    $sshPassword = craft()->goLive_security->decrypt($this->getSettings()['ssh']['remote']['password']);
 
-    $mysqlHostname = $this->_getSettings()['mysql']['hostname'];
-    $mysqlUsername = $this->_getSettings()['mysql']['username'];
-    $mysqlPassword = craft()->goLive_security->decrypt($this->_getSettings()['mysql']['password']);
-    $mysqlDb = $this->_getSettings()['mysql']['dbname'];
+    $mysqlHostname = $this->getSettings()['mysql']['hostname'];
+    $mysqlUsername = $this->getSettings()['mysql']['username'];
+    $mysqlPassword = craft()->goLive_security->decrypt($this->getSettings()['mysql']['password']);
+    $mysqlDb = $this->getSettings()['mysql']['dbname'];
 
     $ssh = new Net_SSH2($sshHostname);
     $ssh->login($sshUsername, $sshPassword);
@@ -170,59 +170,29 @@ class GoLive_TaskService extends BaseApplicationComponent {
   /**
    * Run a user-defined task on the command line
    *
-   * @todo Combine this function and afterImportTask (which are practically identical)
-   *       into a single function.
-   *
    * @param BaseModel $settings The task's settings
    * @param mixed $arg The index of the command to be run
    *
    * @return bool
    */
-  public function beforeBackupTask($settings, $arg = null) {
-    $commands = $this->_getSettings()['beforeBackup']['commands'];
-    $command = $commands[$arg]['command'];
+  public function doSSHTask($settings, $args = null)
+  {
+    $commandSet = $args['commandSet'];
+    $commandEnv = $args['commandEnvironment'];
+    $cwd = $this->getSettings()[$commandSet]['cwd'];
+    $commands = $this->getSettings()[$commandSet]['commands'];
+    $command = $commands[$args['commandIndex']]['command'];
 
-    $sshHostname = $this->_getSettings()['ssh']['local']['hostname'];
-    $sshUsername = $this->_getSettings()['ssh']['local']['username'];
-    $sshPassword = craft()->goLive_security->decrypt($this->_getSettings()['ssh']['local']['password']);
+    $sshHostname = $this->getSettings()['ssh'][$commandEnv]['hostname'];
+    $sshUsername = $this->getSettings()['ssh'][$commandEnv]['username'];
+    $sshPassword = craft()->goLive_security->decrypt($this->getSettings()['ssh'][$commandEnv]['password']);
 
     $ssh = new Net_SSH2($sshHostname);
     $ssh->login($sshUsername, $sshPassword);
 
     // Prepend to any command, as Net_SSH2 doesn't persist state changes
     // across multiple calls to exec()
-    $cdCommand = 'cd ' . $this->_getSettings()['beforeBackup']['cwd'] .'; ';
-
-    $ssh->exec($cdCommand . $command);
-
-    return true;
-  }
-
-
-  /**
-   * Run a user-defined task on the command line
-   *
-   * @param BaseModel $settings The task's settings
-   * @param mixed $arg The index of the command to be run
-   *
-   * @return bool
-   */
-  public function afterImportTask($settings, $arg = null) {
-    $commands = $this->_getSettings()['afterImport']['commands'];
-    $command = $commands[$arg]['command'];
-
-    $sshHostname = $this->_getSettings()['ssh']['remote']['hostname'];
-    $sshUsername = $this->_getSettings()['ssh']['remote']['username'];
-    $sshPassword = craft()->goLive_security->decrypt($this->_getSettings()['ssh']['remote']['password']);
-
-    $ssh = new Net_SSH2($sshHostname);
-    $ssh->login($sshUsername, $sshPassword);
-
-    // Prepend to any command, as Net_SSH2 doesn't persist state changes
-    // across multiple calls to exec()
-    $cdCommand = 'cd ' . $this->_getSettings()['afterImport']['cwd'] .'; ';
-
-    $ssh->exec($cdCommand . $command);
+    $ssh->exec(sprintf('cd %s; %s', $cwd, $command));
 
     return true;
   }
@@ -248,8 +218,12 @@ class GoLive_TaskService extends BaseApplicationComponent {
 
       $task = array(
         'function' => array(
-          'beforeBackupTask',
-          $key
+          'doSSHTask',
+          array(
+            'commandSet' => 'beforeBackup',
+            'commandEnvironment' => 'local',
+            'commandIndex' => $key
+          )
         ),
         'message' => sprintf('Running local task <code>%s</code>...', $command['command'])
       );
@@ -293,8 +267,12 @@ class GoLive_TaskService extends BaseApplicationComponent {
 
       $task = array(
         'function' => array(
-          'afterImportTask',
-          $key
+          'doSSHTask',
+          array(
+            'commandSet' => 'afterImport',
+            'commandEnvironment' => 'remote',
+            'commandIndex' => $key
+          )
         ),
         'message' => sprintf('Running remote task <code>%s</code>...', $command['command'])
       );
@@ -323,7 +301,7 @@ class GoLive_TaskService extends BaseApplicationComponent {
    *
    * @return BaseModel
    */
-  private function _getSettings() {
+  private function getSettings() {
     return craft()->plugins->getPlugin('GoLive')->getSettings();
   }
 }
